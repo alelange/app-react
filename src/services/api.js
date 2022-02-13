4132bc1187b500dc041c1970e0be8d76fbe3dffc
Alexandre Lange <alhala@gmail.com>
2022-02-13 17:53:13 -0300

    Trocando endereço da api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://localhost:44300',
+    // baseURL: 'https://localhost:5001',
+    // rodando app do docker
+    baseURL: 'http://localhost:44300',
 })
 
 export default api;
